85fb0476b852ba015720af5596fde7a99a415246
Juliana <julianamorikoshi@gmail.com>
2020-03-26 15:02:23 -0300

    blah

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,20 @@
 // IMPORTING DEPENDENCIES
 const express = require('express');
 
+/**
+ * ROUTE / CONTROLLERS
+ */
+
+/**
+ * HTTP METHODS:
+ * 
+ * GET POST PUT DELETE
+ */
+
+/**
+ * PARAMETERS
+ */
+
 // CREATE OUR APP
 const app = express();
 app.get('/', (request, response) => {
