e52ee125d58abc9edfd29eeb0db2388e222a10f1
Juliana <julianamorikoshi@gmail.com>
2020-03-28 15:55:26 -0300

    WIP frontend

---
+++ frontend/src/pages/Register/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+// EXPORTS ---------------------------------------------------------------------
+export default function Register() {
+  return (
+    <h1>Register</h1>
+  );
+}
