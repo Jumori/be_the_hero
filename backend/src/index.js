10dab5ea618cbf74ac626a2f1cfd0daf54f4805c
Juliana Morikoshi <juliana@suthub.com>
2020-03-29 15:58:49 -0300

    API validation

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 // IMPORTING DEPENDENCIES ------------------------------------------------------
 const express = require('express');
 const cors = require('cors');
+const { errors } = require('celebrate');
 
 // IMPORTING FILES -------------------------------------------------------------
 const routes = require('./routes');
@@ -35,7 +36,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(routes);
-
+app.use(errors());
 
 // LISTEN TO PORT 3333
 app.listen(3333);
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { celebrate, Segments, Joi } = require('celebrate');
 
 const sessionController = require('./controllers/sessionController');
 const ongController = require('./controllers/ongController');
@@ -8,16 +9,62 @@ const profileController = require('./controllers/profileController.js');
 const routes = express.Router();
 
 // ROUTES ----------------------------------------------------------------------
-routes.post('/sessions', sessionController.create);
+// SESSION ROUTES --------------------------------------------------------------
+routes.post('/sessions', celebrate({
+  [Segments.BODY]: Joi.object().keys({
+    login: Joi.object().keys({
+      id: Joi.string().required()
+    })
+  })
+}), sessionController.create);
 
+// ONGS ROUTES -----------------------------------------------------------------
 routes.get('/ongs', ongController.index);
-routes.post('/ongs', ongController.create);
 
-routes.get('/incidents', incidentController.index);
-routes.post('/incidents', incidentController.create);
-routes.delete('/incidents/:id', incidentController.delete);
+routes.post('/ongs', celebrate({
+  [Segments.BODY]: Joi.object().keys({
+    ong: Joi.object().keys({
+      name: Joi.string().required(),
+      email: Joi.string().required().email(),
+      phone: Joi.number().required().min(10).max(11),
+      city: Joi.string().required(),
+      uf: Joi.string().required().length(2)
+    })
+  })
+}), ongController.create);
 
-routes.get('/profile', profileController.index);
+// INCIDENTS ROUTES ------------------------------------------------------------
+routes.get('/incidents', celebrate({
+  [Segments.QUERY]: Joi.object().keys({
+    page: Joi.number()
+  })
+}), incidentController.index);
+
+routes.post('/incidents', celebrate({
+  [Segments.HEADERS]: Joi.object({
+    authorization: Joi.string().required(),
+  }).unknown(),
+  [Segments.BODY]: Joi.object().keys({
+    incident: Joi.object().keys({
+      title: Joi.string().required(),
+      description: Joi.string().required(),
+      value: Joi.number().required()
+    })
+  })
+}), incidentController.create);
+
+routes.delete('/incidents/:id', celebrate({
+  [Segments.PARAMS]: Joi.object().keys({
+    id: Joi.number().required()
+  })
+}), incidentController.delete);
+
+// PROFILE ROUTES --------------------------------------------------------------
+routes.get('/profile', celebrate({
+  [Segments.HEADERS]: Joi.object({
+    authorization: Joi.string().required(),
+  }).unknown()
+}), profileController.index);
 
 
 // EXPORTS ---------------------------------------------------------------------
